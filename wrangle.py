2aee155aa1b897999a16a8962f8330be036daca9
Cody Clonts <clontscody@gmail.com>
2022-06-29 16:53:53 -0500

    wrangle file updated

--- wrangle.py
+++ wrangle.py
@@ -18,3 +18,26 @@ def get_acnh_data():
     if os.path.isfile(filename):
         df = pd.read_csv(filename, index_col = 0)
         return df
+
+
+
+def successful(tier):
+    if tier == 1 or tier == 2 or tier == 3:
+        return 1
+    else:
+        return 0
+
+
+
+    
+def prep_acnh_data(df):
+    columns = ['birthday', 'catchphrase', 'wallpaper', 'flooring', 'filename', 'furniture_list', 'unique_entry_id']
+    df.columns = df.columns.str.lower()
+    df.columns = df.columns.str.replace( ' ' , '_')
+    df.drop(columns,inplace = True, axis = 1)
+    df["is_successful"] = df["tier"].apply(lambda tier: successful(tier))
+    df = df.rename(columns={'rank':'position'})
+    return df
+
+
+
